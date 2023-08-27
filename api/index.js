702f5f2a1aefb4d15f54e53cafed7e8aafc624e3
Sam Grzelak <64333101+Samuel-Grzelak@users.noreply.github.com>
2023-08-26 22:38:53 -0500

    Port Updates 1
    Updating ports and start for index.js file to get deployment working on Heroku.

--- api/index.js
+++ api/index.js
@@ -29,6 +29,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(5000, () => {
-  console.log("Connected!");
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
