a81c3834f987fd4140ff55cd362b3c0a14ccebb4
Sam Grzelak <64333101+Samuel-Grzelak@users.noreply.github.com>
2023-08-27 09:33:44 -0500

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password: "password", // process.env.DB_KEY,
-  database:"blog"
+  host:"us-cdbr-east-06.cleardb.net",
+  user:"b12d58d1d827b8",
+  password: "bd6af01c",
+  database:"heroku_9c381a962076a24"
 })
